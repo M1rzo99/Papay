084df146d12a2bf53f99080f7c417ed119eb635b
Mirzo/미조 <mirzo@mijos-MacBook-Pro.local>
2023-10-26 22:08:00 +0900

    feat:Schema & AddNewProduct and updateChoosenProduct

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,5 @@
-const { shapeIntoMongooseObjectedId } = require("../lib/config");
 const assert = require("assert");
+const { shapeIntoMongooseObjectId } = require("../lib/config");
 const Definer = require("../lib/mistakes");
 const ProductModel = require("../schema/product.model");
 
@@ -10,13 +10,31 @@ class Product {
   async addNewProductData(data, member) {
     try {
       // member_id ni  monngoDB obj idga aylantirish;
-      data.restaurant_mb_id = shapeIntoMongooseObjectedId(member._id);
+      data.restaurant_mb_id = shapeIntoMongooseObjectId(member._id);
+      console.log(data.restaurant_mb_id);
       console.log(data);
 
       const new_product = new this.productModel(data);
       const result = await new_product.save();
       assert.ok(result, Definer.product_err1);
 
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  }
+  async updateChosenProductData(id, updated_data, mb_id) {
+    try {
+      id = shapeIntoMongooseObjectId(id);
+      mb_id = shapeIntoMongooseObjectId(mb_id);
+      const result = await this.productModel
+        .findOneAndUpdate({ _id: id, restaurant_mb_id: mb_id }, updated_data, {
+          runValidators: true,
+          lean: true,
+          returnDocument: "after",
+        })
+        .exec();
+      assert.ok(result, Definer.general_err1);
       return result;
     } catch (err) {
       throw err;
